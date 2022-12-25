35ad79d91c8fa87f652d8499ff288eac98e361c2
Alexandr Hrynenko <layt.lat@ukr.net>
2022-12-25 14:13:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-//   function toggleModal() {
-//     refs.modal.classList.toggle("backdrop--is-hidden");
-//   }
-// })();
-// @import './js/modal.js';
+  function toggleModal() {
+    refs.modal.classList.toggle("backdrop--is-hidden");
+  }
+})();
+import './js/modal.js';
